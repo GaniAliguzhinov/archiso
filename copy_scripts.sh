5f0664e1714b752490d9d4b10d4a03089614a8ac
Gani Aliguzhinov <gani.aliguzhinov@gmail.com>
2021-07-10 18:19:17 +0600

    nerds

--- copy_scripts.sh
+++
@@ -1,2 +0,0 @@
-# cp /home/galiguzhinov/i3-arch/*.sh airootfs/root/
-echo -e "#!/usr/bin/env bash\ngit clone https://github.com/GaniAliguzhinov/i3-arch.git && cp i3-arch/*.sh ." > airootfs/root/copy.sh
---
+++ get_nerds.sh
@@ -0,0 +1,3 @@
+wget -O airootfs/root/nerd-fonts-2.1.0.tar.gz https://github.com/ryanoasis/nerd-fonts/archive/v2.1.0.tar.gz
+--2021-07-10 18:04:45--  https://github.com/ryanoasis/nerd-fonts/archive/v2.1.0.tar.gz
+Loaded CA certificate '/etc/ssl/certs/ca-certificates.crt'
